e0699e5434214c8d8ccaec3c41f974ee869b17af
Changhwan Kim <kimch061279@gmail.com>
2026-04-24 12:48:32 +0900

    edit message_logistics.py; Update request_headers

--- message_util/message_logistics.py
+++ message_util/message_logistics.py
@@ -11,10 +11,10 @@ import requests
 dotenv.load_dotenv()
 
 request_headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                    "AppleWebKit/537.36 (KHTML, like Gecko) "
-                    "Chrome/71.0.3578.98 Safari/537.36",
-    "Content-Type": "application/x-www-form-urlencoded"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": "keep-alive"
 }
 
 logistics_urls = {
